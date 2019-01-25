25767e23b182275fec86bd6c75b9b31306377825
NoeHedde <NoeHedde@users.noreply.github.com>
2019-01-25 23:21:25 +0100

    rebounds

---
+++ baby_story/Assets/Script/Speed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+
+    public float velX;
+    public float velZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(velX, 0, velZ), ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
